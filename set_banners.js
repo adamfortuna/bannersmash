8f51fae6da24d48a9c2e8113a486c1000a5914ff
Adam Fortuna <adam@fortuna.name>
2012-11-11 12:08:55 -0500

    Split out cars and shoes

--- set_banners.js
+++ set_banners.js
@@ -60,28 +60,26 @@ var shoes  = [
 ];
 
 
-/*
-var cars = [
-  { 
-    type: 'convertible', 
-    link: 'images/select/convertible_roadblock_txt.jpg'
-  },
-  { 
-    type: 'sedan',
-    link: 'images/select/sedan_roadblock_txt.jpg'
-  },
-  {
-    type: 'suv',
-    link: 'images/select/suv_roadblock_text_shading_20.jpg'
-  }
-];
-*/
+
 function setBanners(number) {
-  car = cars[number];
+  car = choices[number];
 
   var headerAd = $("<a href='"+car.link+"'><img src='images/"+car.type+"_banner.jpg' /></a>");
   var sideAd = $("<a href='"+car.link+"'><img src='images/"+car.type+"_sidebar.jpg' /></a>");
 
   $("#headerPromoContainer").hide().html(headerAd).fadeIn('slow');
   $("#ad0_0_WA_0001A").hide().html(sideAd).fadeIn('slow');
+}
+
+
+
+function get_advertisment(i)
+{
+  
+var advert  = choices[i];
+
+return advert;
+
+
+  
 }
